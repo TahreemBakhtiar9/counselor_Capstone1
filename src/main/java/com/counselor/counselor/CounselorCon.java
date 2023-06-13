2beeff2fe7e814fd53ffc5bed6cd1ed7a3992f4b
TahreemBakhtiar9 <tehreembakhtiar927@gmail.com>
2023-06-13 12:36:40 +0500

    CORS

--- src/main/java/com/counselor/counselor/CounselorCon.java
+++ src/main/java/com/counselor/counselor/CounselorCon.java
@@ -2,6 +2,7 @@ package com.counselor.counselor;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.persistence.Id;
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/counselor")
 public class CounselorCon {
